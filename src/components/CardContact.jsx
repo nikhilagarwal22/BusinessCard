fd90ee800ba5cd6d6abfdffd570011e313a21aaa
Manoj Rawat <nikhil.blm@gmail.com>
2024-02-18 22:09:43 +0530

    upi logo

--- src/components/CardContact.jsx
+++ src/components/CardContact.jsx
@@ -11,12 +11,12 @@ const CardContact = () => {
             <p><a href={`tel:+917753955555`} target="_blank" rel="noreferrer">+917753955555</a></p>
         </div>
         <div className="contact-details-item">
-            <h3>E-mail Id</h3>
+            <h3>Email Id</h3>
             <p><a href="mailto:knmill1991@gmail.com" target="_blank" rel="noreferrer">knmill1991@gmail.com</a></p>
         </div>
         <div className="contact-details-item">
             <h3>Factory Address</h3>
-            <p><a href="https://maps.app.goo.gl/Le6ycfesnniM98yY7" target="_blank" rel="noreferrer">A-10, Industrial Area Phase ll<br />Sandila, Distt- Hardoi, 241127, U.P.</a></p><br />
+            <p><a href="https://maps.app.goo.gl/Le6ycfesnniM98yY7" target="_blank" rel="noreferrer">A-10, Industrial Area Phase-II<br />Sandila, Distt- Hardoi, 241127, U.P.</a></p><br />
             <h3>Head Office Address</h3>
             <p><a href="https://maps.app.goo.gl/sbo7Uvb2fciEQMp7A" target="_blank" rel="noreferrer">Kachhauna, Distt-Hardoi, 241126, U.P.</a></p><br />
             
